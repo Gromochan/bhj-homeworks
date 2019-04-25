dca49f6d754cf27341afff6912203b253f1525f9
Ivan Petrov <kepocka@gmail.com>
2019-04-26 02:04:01 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,34 +1,46 @@
 leftArrow = document.getElementsByClassName("slider__arrow_prev").item(0);
 rightArrow = document.getElementsByClassName("slider__arrow_next").item(0);
+
+
+const worker = function (side) {
+    let image = document.getElementsByClassName("slider__item slider__item_active").item(0);
+    if (side === "left") {
+        if (image.previousElementSibling) {
+            image.previousElementSibling.classList.add("slider__item_active");
+        } else {
+            image.parentElement.lastElementChild.classList.add("slider__item_active")
+        }
+    }
+    if (side === "right") {
+        if (image.nextElementSibling) {
+            image.nextElementSibling.classList.add("slider__item_active");
+        } else {
+            image.parentElement.firstElementChild.classList.add("slider__item_active")
+        }
+    }
+    image.classList.remove("slider__item_active");
+
+}
 leftArrow.onclick = function () {
-    let image = document.getElementsByClassName("slider__item slider__item_active").item(0);
-    if (image.previousElementSibling) {
-        image.previousElementSibling.classList.add("slider__item_active");
-    } else {
-        image.parentElement.lastElementChild.classList.add("slider__item_active")
-    }
-    image.classList.remove("slider__item_active");
-}
+    worker("left")
+};
 rightArrow.onclick = function () {
-    let image = document.getElementsByClassName("slider__item slider__item_active").item(0);
-    if (image.nextElementSibling) {
-        image.nextElementSibling.classList.add("slider__item_active");
-    } else {
-        image.parentElement.firstElementChild.classList.add("slider__item_active")
-    }
-    image.classList.remove("slider__item_active");
-}
+    worker("right")
+};
+
 
 dotNav = () => {
     const dot = document.getElementsByClassName("slider__dot");
     for (i = 0; dot.length > i; i++) {
-        console.log(i)
-        // Не могу понять, почему  данный цикл не работает. По идее, он должен присваивать каждому отдельному объекту свой собственный onclick со своим собственным значением i, но в итоге все получают одно и то же, последнее значение, которое даже получить не должно быть возможным. Почему console.log внутри
+        var i;
+        let f = i;
         dot.item(i).onclick = function () {
-            console.log(i)
-            document.getElementsByClassName("slider__item").item(i).classList.add("slider__item_active");
-            document.getElementsByClassName("slider__item_active").item(0).classList.remove("slider__item_active")
-            dot.item(i).classList.add("slider__dot_active");
+            if (document.getElementsByClassName("slider__dot_active").length > 0) {
+                document.getElementsByClassName("slider__dot_active").item(0).classList.toggle("slider__dot_active")
+            }
+            dot.item(f).classList.toggle("slider__dot_active");
+            document.getElementsByClassName("slider__item_active").item(0).classList.toggle("slider__item_active");
+            document.getElementsByClassName("slider__item").item(f).classList.toggle("slider__item_active");
         }
     }
 }
