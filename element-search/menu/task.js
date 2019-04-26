8827276fd6bd7cb3a25bff5a1955d57ffee0a81e
Ivan Petrov <kepocka@gmail.com>
2019-04-26 11:42:38 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,10 @@
 const menu = document.getElementsByClassName("menu__item");
 handler = (e) => {
-    const menuItem = e.target.nextElementSibling;
+
+    const menuItem = e.target.nextElementSibling
     if (menuItem) {
-        menuItem.classList.add("menu_active")
+        e.preventDefault();
+        menuItem.classList.toggle("menu_active")
     }
 }
 onclick = handler;
-
-// не могу понять почему меню сразу закрывается. Раз оно открывается, значит класс присваевается нужному элементу, а в коде я не закрываю меню.
