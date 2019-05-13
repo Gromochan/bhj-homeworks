945cf26441456f5774040a923244f381d41cfaf9
Ivan Petrov <kepocka@gmail.com>
2019-05-13 22:19:56 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,10 +1,13 @@
 const formData = new FormData(document.getElementById("form"));
 const req = new XMLHttpRequest();
+const progress = document.getElementById("progress");
 form.addEventListener('submit', e => {
     e.preventDefault();
     req.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
     req.send(formData);
     const upload = req.upload;
-    upload.addEventListener("onprogress", (console.log(upload)))
+    upload.onprogress = function (event) {
+        progress.value = `${event.loaded*100/event.total}`;
+    }
 
 })
