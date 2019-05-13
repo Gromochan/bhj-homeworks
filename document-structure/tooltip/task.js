b3a5efc619c3bfa014138eca32f06883fb19c6bf
Ivan Petrov <kepocka@gmail.com>
2019-05-13 21:32:38 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,10 +5,19 @@ for (let i = 0; element.length > i; i++) {
     let f = i;
     element.item(f).onclick = function (e) {
         e.preventDefault();
-        console.log(element.item(f).title)
-        tooltip.textContent = element.item(f).title;
-        tooltip.setAttribute("data-position", "left");
-        element.item(f).insertAdjacentElement("afterend", tooltip)
-        tooltip.classList.toggle("tooltip-active");
+        if (tooltip.classList.contains("tooltip-active")) {
+            tooltip.classList.toggle("tooltip-active");
+            tooltip.style.display = "none"
+        } else {
+            console.log(element.item(f).title)
+            tooltip.textContent = element.item(f).title;
+            tooltip.setAttribute("data-position", "left");
+            element.item(f).insertAdjacentElement("afterend", tooltip);
+            tooltip.classList.toggle("tooltip-active");
+            tooltip.style.display = "inline-block";
+            tooltip.style.position = "absolute";
+            tooltip.style.marginTop = "20px";
+        }
+
     }
 }
